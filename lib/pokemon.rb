7cc4dc7fb2def8bc516ea418fac43b6c573889f0
Dave Winniford <dwinniford@gmail.com>
2020-02-12 19:54:05 +0000

    Automatically backed up by Learn

--- lib/pokemon.rb
+++ lib/pokemon.rb
@@ -1,2 +1,9 @@
 class Pokemon
+  attr_accessor :id, :name, :grade, :db 
+  def initialize(id = nil, name:, type:, db:)
+    @id = id 
+    @name = name 
+    @type = type 
+    @db = db 
+  end 
 end
